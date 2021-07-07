a7d56db17f1784fd34ba6f98d7be48453a4b514a
Vytautas <vytautaskaralius@gmail.com>
2021-07-07 20:24:33 +0300

    Last fixings

--- game.py
+++ game.py
@@ -57,50 +57,44 @@ def set_figure(entered_coords, game_matrix, current_figure):
 
     # check elements count
     if len(coords_values) != 2:
-        result = f"Непонятны координаты {entered_coords}"
+        result = f"Непонятны координаты '{entered_coords}'."
     elif len(coords_values) == 2:
         try:
+            #пробуем конвертировать координаты в число
             x_coord = int(coords_values[0])
             y_coord = int(coords_values[1])
         except:
-            result = f"Непонятны координаты {entered_coords}"
+            # конвертировать не удалось
+            result = f"Непонятны координаты '{entered_coords}'."
 
     if result == "OK":
+        # проверка координат на принадлежность правильному интервалу
         if not 0 < x_coord < 4 or not 0 < y_coord < 4:
-            result = f"Координаты {entered_coords} выходят за пределы допустимых (1-3<Разделитель>1-3)."
+            result = f"Координаты '{entered_coords}' выходят за пределы допустимых (1-3<Разделитель>1-3)."
 
     if result == "OK":
         if game_matrix[x_coord - 1][y_coord - 1].strip() == '':
+            # присваиваем символ ячейке
             game_matrix[x_coord - 1][y_coord - 1] = current_figure
         else:
-            result = "Ячейка уже заполнена!"
+            # ячейка уже заполнена - пишем сообщение об ошибке
+            result = f"Ячейка '{entered_coords}' уже заполнена!"
 
     return result
 
 
 # функция проверки есть ли победитель
 def is_win(game_matrix, current_player_figure):
-    win_text = ""
 
     figure_won_rows, figure_won_cols, figure_won_diagonals = False, False, False
 
     # проверка строк на выигрошную комбинацию
-    """
-    for row in game_matrix:
-        if not figure_won:
-            figure_won = (row[0] == current_player_figure and row[1] == current_player_figure and row[
-                2] == current_player_figure)
-        else:
-            break
-    """
     figure_won_rows = len(list(filter(lambda x: (len(list(filter(lambda y: y == current_player_figure, x))) == 3), game_matrix))) > 0
 
     # проверка столбцов на выигрошную комбинацию
     for i in range(0, 3):
         if not figure_won_cols:
             figure_won_cols = len(list(filter(lambda x: x[i] == current_player_figure ,game_matrix))) == 3
-                #(game_matrix[i][0] == current_player_figure and game_matrix[i][1] == current_player_figure and
-                          #game_matrix[i][2] == current_player_figure)
         else:
             break
 
@@ -160,6 +154,8 @@ while not second_player:
 # инициализируем игру
 init_game(first_player, second_player)
 
+# флаг прерывания игры
+break_game = False
 
 while True:
     # ждём хода текущего игрока
@@ -173,16 +169,24 @@ while True:
 
     # прерываем игру и выходим из программы
     if coords.lower().strip() == "exit":
+        break_game = True
+
+    if not break_game:
+        # присаваиваем фигуру ячейке матрицы и возвращаем результат присвоения
+        set_result = set_figure(coords, game_matrix, current_player_figure)
+
+        while set_result != "OK":
+            # введены неверные или непонятные координаты ячейки - ждём правильных координат ячейки
+            coords = input(set_result + " Введите веpные координаты ячейки (формат: 1-3<Разделитель>1-3):")
+            if coords.lower().strip() == "exit":
+                break_game = True
+                break #прерываем ввод координат
+            set_result = set_figure(coords, game_matrix, current_player_figure)
+
+    # прерываем игру и выходим из программы
+    if break_game:
         print("Игра прервана!!!!")
         break
-
-    # присаваиваем фигуру ячейке матрицы и возвращаем результат присвоения
-    set_result = set_figure(coords, game_matrix, current_player_figure)
-
-    while set_result != "OK":
-        # введены неверные или непонятные координаты ячейки - ждём правильных координат ячейки
-        coords = input(set_result + " Введите веpные координаты ячейки (1-3<Разделитель>1-3):")
-        set_result = set_figure(coords, game_matrix, current_player_figure)
 
     if set_result == "OK":
         # проверяем есть ли победитель
